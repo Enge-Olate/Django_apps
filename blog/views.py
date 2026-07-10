ebb7be9477d2864879fc03e663e8554e6feb0221
Enge-Olate <marcioalexisolate@live.com>
2026-07-10 11:43:11 -0300

    Primeiro commit: adicionando path ao urls.py, implementando class PostView e testando a view.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
+class PostView(generic.View):
+    def get(self, request):
+        return HttpResponse("Hello, World!")
+    
